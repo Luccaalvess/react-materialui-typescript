5d4f8f90e6be19ac6c9a2b500a8de835a3c4d247
Luccaalvess <lucca-alves@hotmail.com>
2025-07-16 22:37:35 -0300

    feat: Adicionado react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-// import React from 'react';
+import { BrowserRouter } from "react-router";
+import { AppRoutes } from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      Teste
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 

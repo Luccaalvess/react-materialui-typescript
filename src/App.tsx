24be72d45dd3f008a032811cb8c5df16e4802393
Luccaalvess <lucca-alves@hotmail.com>
2025-08-27 21:33:05 -0300

    feat: adicionado responsividade no menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes";
 // import { ThemeProvider } from "@mui/material";  
 // import { LightTheme } from "./shared/themes";
-import { AppThemeProvider } from "./shared/contexts";
+import { AppThemeProvider, DrawerProvider } from "./shared/contexts";
 import { MenuLateral } from "./shared/components";
 
 
 function App() {
   return (
     <AppThemeProvider>
-      <BrowserRouter>
-        <MenuLateral>
-          <AppRoutes />
-        </MenuLateral>
-      </BrowserRouter>
+      <DrawerProvider>
+        <BrowserRouter>
+          <MenuLateral>
+            <AppRoutes />
+          </MenuLateral>
+        </BrowserRouter>
+      </DrawerProvider>
     </AppThemeProvider>
   );
 }
--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,9 +1,8 @@
-import { Avatar, Box, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material";
+import { Avatar, Box, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material";
 import React, { ReactNode } from "react";
 
 import HomeIcon from '@mui/icons-material/Home';
-
-
+import { useDrawerContext } from "../../contexts";
 
 interface IMenuLateralProps {
     children: ReactNode;
@@ -12,11 +11,13 @@ interface IMenuLateralProps {
 export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
 
     const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const {isDrawerOpen, toggleDrawerOpen} = useDrawerContext();
 
     return (
         <>
-
-            <Drawer variant="permanent">
+            <Drawer open={isDrawerOpen} variant={smDown ? "temporary" : "permanent"} onClose={toggleDrawerOpen}>
                 <Box width={theme.spacing(28)} height='100%' display='flex' flexDirection='column'>
 
 
@@ -36,15 +37,12 @@ export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
                             </ListItemButton>
                         </List>
                     </Box>
-
-
                 </Box>
             </Drawer >
 
-
-            <Box height='100vh' marginLeft={theme.spacing(28)}>
+            <Box height='100vh' marginLeft={smDown ? 0 : theme.spacing(28)}>
                 {children}
             </Box>
         </>
-    )
-}
+    );
+}; 
